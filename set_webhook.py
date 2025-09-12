b2c9b1187589e7f51ef9f880069669e750d9e759
Roman-Shulpov <romkakub@mail.ru>
2025-09-13 00:59:51 +0700

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -1,16 +1,33 @@
+# ========================РАБОТАЕТ ВРОДЕ НО НЕ ТАК===========================================
+# import os
+# from aiogram import Bot
+# import asyncio
+
+# TOKEN = os.getenv("TOKEN")
+# WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+
+# bot = Bot(token=TOKEN)
+
+# async def main():
+#     await bot.delete_webhook()
+#     await bot.set_webhook(WEBHOOK_URL)
+#     print(f"Webhook установлен: {WEBHOOK_URL}")
+
+# if __name__ == "__main__":
+#     asyncio.run(main())
+
+# ========================РАБОТАЕТ ВРОДЕ НО НЕ ТАК===========================================
 import os
-from aiogram import Bot
 import asyncio
+from aiogram import Bot
 
-TOKEN = os.getenv("TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-
-bot = Bot(token=TOKEN)
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEBHOOK_URL = f"https://bmx-bot-hual.onrender.com/webhook/{BOT_TOKEN}"
 
 async def main():
-    await bot.delete_webhook()
+    bot = Bot(token=BOT_TOKEN)
     await bot.set_webhook(WEBHOOK_URL)
-    print(f"Webhook установлен: {WEBHOOK_URL}")
+    print(f"Webhook установлен на {WEBHOOK_URL}")
 
 if __name__ == "__main__":
     asyncio.run(main())
