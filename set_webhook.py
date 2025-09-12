0e95f79503e8365e602c45a5c8da2f3a0b0fd753
Roman-Shulpov <romkakub@mail.ru>
2025-09-13 00:51:33 +0700

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -1,18 +1,16 @@
 import os
+from aiogram import Bot
 import asyncio
-from aiogram import Bot, Dispatcher
 
 TOKEN = os.getenv("TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # Например: https://yourapp.onrender.com/webhook
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher()
 
 async def main():
-    await bot.delete_webhook(drop_pending_updates=True)  # удаляем старый webhook
+    await bot.delete_webhook()
     await bot.set_webhook(WEBHOOK_URL)
     print(f"Webhook установлен: {WEBHOOK_URL}")
-    await bot.session.close()
 
 if __name__ == "__main__":
     asyncio.run(main())
