3ac9acbc0ec6a1078fd879c73ec8e9f4b4fbb9f3
Roman-Shulpov <romkakub@mail.ru>
2025-09-13 00:06:30 +0700

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -1,13 +1,13 @@
+import os
 import asyncio
 from aiogram import Bot
 
-TOKEN = "7976564635:AAGr4yMj4jDk5Lu6wam9JOfvkSrwHw0eYzg"
-WEBHOOK_URL = f"https://bmx-bot-hual.onrender.com/webhook/{TOKEN}"
+TOKEN = os.getenv("TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 async def main():
     bot = Bot(token=TOKEN)
-    await bot.set_webhook(WEBHOOK_URL)
-    print(f"Webhook установлен: {WEBHOOK_URL}")
-    await bot.session.close()
+    await bot.set_webhook(f"{WEBHOOK_URL}/webhook/{TOKEN}")
+    print("Webhook установлен вручную")
 
 asyncio.run(main())
