3f536d8d1ac9cac7314a5cab2dfd9f63bee9532a
Roman-Shulpov <romkakub@mail.ru>
2025-09-13 11:09:08 +0700

    Delete app directory

--- app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # Должно быть HTTPS
-
-CHAT_ID_1 = int(os.getenv("CHAT_ID_1"))
-CHAT_ID_2 = int(os.getenv("CHAT_ID_2"))
-ALLOWED_CHAT_IDS = {CHAT_ID_1, CHAT_ID_2}
